bd428a92ea657460cad66f374795d33e35507841
aissataikyzy <90189038+aissataikyzy@users.noreply.github.com>
2023-09-20 01:14:10 +0600

    Update 17.py

---
+++ hw2/17.py
@@ -0,0 +1,4 @@
+print("Sample List: ")
+datalist = [1452, 11.23, 1+2j, True, 'student', (0, -1), [5, 12], {"class":'V', "section":'A'}]
+for item in datalist:
+   print ("Type of ", item, " is ", type(item))
