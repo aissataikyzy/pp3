6d29553849db0fb73634359bf63a8c056376ef87
aissataikyzy <90189038+aissataikyzy@users.noreply.github.com>
2023-09-20 12:30:31 +0600

    Update 1.py

--- hw2/1.py
+++ hw2/1.py
@@ -1,5 +1,5 @@
 def ooo():
-    str="";
+    str=""
     for row in range(0, 7):
         for col in range(0, 7):
             if (((col==1 or col==5) and row!=0 and row!=6) or ((row==0 or row==6) and col>1 and col<5)):
@@ -7,4 +7,4 @@ def ooo():
             else:
                 str=str+" "
         str=str+"\n"
-    print(str);
+    print(str)
