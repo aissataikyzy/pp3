3e368e153e4400c0b547703c16cfdb2b18efe04b
aissataikyzy <90189038+aissataikyzy@users.noreply.github.com>
2023-11-28 12:19:11 +0600

    Update 5.py

--- hw10/5.py
+++ hw10/5.py
@@ -1,9 +1,14 @@
-import numpy as np
 import matplotlib.pyplot as plt
-labels = ['Ireland','Iceland','South Korea','Germany','US','Italy']
-values = [17.7,10.7,10.2,10.2,21,30.3]
-colors = ['#be5168','#e16552','#e2975d','#e9d78e','#3c8e9d','#993767']
-plt.pie(values,colors=colors,autopct='%1.1f%%',wedgeprops = {"edgecolor" : "white",'linewidth': 1,'antialiased': True},startangle=145,textprops={'color':"w"})
-plt.legend(labels,loc ="center left",bbox_to_anchor =(0.83, 0.5, 0, 0))
-plt.axis('equal')
+import numpy as np
+
+y = np.array([30.3, 21, 10.2, 10.2, 10.7, 17.7, 3.6, 3.8, 4.1, 4.2, 5.3])
+
+labels = ["Italy", "Spain", "Germany", "Greece", "Iceland", "Ireland", "Austria", "Belgium", "Denmark", "Finland", "France" ]
+plt.title("Countries")
+
+colors = ["MediumVioletRed", "DarkCyan", "DarkOliveGreen", "GoldenRod", "Ivory", "LightCoral", "MediumOrchid", "MediumSeaGreen", "MistyRose", "PLum", "LemonChiffon"]
+
+plt.pie(y, colors=colors, autopct="%.1f%%", textprops=dict(fontsize=8))
+plt.legend(labels=labels, loc='upper center',
+           bbox_to_anchor=(0.5, 0.05), ncol=5 )
 plt.show()
